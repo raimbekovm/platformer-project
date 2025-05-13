7f0cd2bac26a5a7e23781aac9958858a0c32ab17
raimbekovm <raimbekov_m@auca.kg>
2025-05-13 17:25:15 +0600

    Implemented level loading, unloading and cell access functions

---
+++ level.cpp
@@ -0,0 +1,46 @@
+#include "level.h"
+
+bool is_inside_level(int row, int column) {
+    if (row < 0 || row >= current_level.rows) return false;
+    if (column < 0 || column >= current_level.columns) return false;
+    return true;
+}
+
+bool is_colliding(Vector2 pos, char look_for) {
+    Rectangle entity_hitbox = {pos.x, pos.y, 1.0f, 1.0f};
+
+    for (int row = pos.y - 1; row < pos.y + 1; ++row) {
+        for (int column = pos.x - 1; column < pos.x + 1; ++column) {
+            if (!is_inside_level(row, column)) continue;
+            if (get_level_cell(row, column) == look_for) {
+                Rectangle block_hitbox = {(float) column, (float) row, 1.0f, 1.0f};
+                if (CheckCollisionRecs(entity_hitbox, block_hitbox)) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+char& get_collider(Vector2 pos, char look_for) {
+    Rectangle player_hitbox = {pos.x, pos.y, 1.0f, 1.0f};
+
+    for (int row = pos.y - 1; row < pos.y + 1; ++row) {
+        for (int column = pos.x - 1; column < pos.x + 1; ++column) {
+            if (!is_inside_level(row, column)) continue;
+            if (get_level_cell(row, column) == look_for) {
+                Rectangle block_hitbox = {(float) column, (float) row, 1.0f, 1.0f};
+                if (CheckCollisionRecs(player_hitbox, block_hitbox)) {
+                    return get_level_cell(row, column);
+                }
+            }
+        }
+    }
+
+    return get_level_cell(pos.x, pos.y);
+}
+
+void reset_level_index() {
+    level_index = 0;
+}
