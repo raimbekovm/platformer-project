9fa69ab17cc2508d8161ef4d792c6b9dd5f896a6
raimbekovm <raimbekov_m@auca.kg>
2025-05-13 23:41:52 +0600

    Implemented player physics

--- player.cpp
+++ player.cpp
@@ -1,43 +1,70 @@
 #include "player.h"
 
-void reset_player_stats() {
-    player_lives = MAX_PLAYER_LIVES;
-    for (int i = 0; i < LEVEL_COUNT; i++) {
-        player_level_scores[i] = 0;
+void move_player_horizontally(float delta) {
+    float next_x = player_pos.x + delta;
+    if (!is_colliding({next_x, player_pos.y}, WALL)) {
+        player_pos.x = next_x;
+    } else {
+        player_pos.x = roundf(player_pos.x);
+        return;
+    }
+
+    is_looking_forward = delta > 0;
+    if (delta != 0) is_moving = true;
+}
+
+void update_player_gravity() {
+    if (is_colliding({player_pos.x, player_pos.y - 0.1f}, WALL) && player_y_velocity < 0) {
+        player_y_velocity = CEILING_BOUNCE_OFF;
+    }
+
+    player_pos.y += player_y_velocity;
+    player_y_velocity += GRAVITY_FORCE;
+
+    is_player_on_ground = is_colliding({player_pos.x, player_pos.y + 0.1f}, WALL);
+    if (is_player_on_ground) {
+        player_y_velocity = 0;
+        player_pos.y = roundf(player_pos.y);
     }
 }
 
-void increment_player_score() {
-    PlaySound(coin_sound);
-    player_level_scores[level_index]++;
-}
+void update_player() {
+    update_player_gravity();
 
-int get_total_player_score() {
-    int sum = 0;
-    for (int i = 0; i < LEVEL_COUNT; i++) {
-        sum += player_level_scores[i];
+    if (is_colliding(player_pos, COIN)) {
+        get_collider(player_pos, COIN) = AIR;
+        increment_player_score();
     }
-    return sum;
-}
 
-void spawn_player() {
-    player_y_velocity = 0;
-    for (size_t row = 0; row < current_level.rows; ++row) {
-        for (size_t column = 0; column < current_level.columns; ++column) {
-            char cell = get_level_cell(row, column);
-            if (cell == PLAYER) {
-                player_pos.x = column;
-                player_pos.y = row;
-                set_level_cell(row, column, AIR);
-                return;
+    if (is_colliding(player_pos, EXIT)) {
+        if (timer > 0) {
+            timer -= 25;
+            time_to_coin_counter += 5;
+
+            if (time_to_coin_counter / 60 > 1) {
+                increment_player_score();
+                time_to_coin_counter = 0;
             }
+        } else {
+            load_level(1);
+            PlaySound(exit_sound);
+        }
+    } else {
+        if (timer >= 0) timer--;
+    }
+
+    if (is_colliding(player_pos, SPIKE) || player_pos.y > current_level.rows) {
+        kill_player();
+    }
+
+    if (is_colliding_with_enemies(player_pos)) {
+        if (player_y_velocity > 0) {
+            remove_colliding_enemy(player_pos);
+            PlaySound(kill_enemy_sound);
+            increment_player_score();
+            player_y_velocity = -BOUNCE_OFF_ENEMY;
+        } else {
+            kill_player();
         }
     }
-}
-
-void kill_player() {
-    PlaySound(player_death_sound);
-    game_state = DEATH_STATE;
-    player_lives--;
-    player_level_scores[level_index] = 0;
 }
--- player.h
+++ player.h
@@ -3,10 +3,14 @@
 
 #include "globals.h"
 
+// Function declarations
 void reset_player_stats();
 void increment_player_score();
 int get_total_player_score();
 void spawn_player();
 void kill_player();
+void move_player_horizontally(float delta);
+void update_player_gravity();
+void update_player();
 
 #endif // PLAYER_H
